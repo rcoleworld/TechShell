226d704c702261423e4f702bb052b8b9909dbdc3
rcoleworld <35586435+rcoleworld@users.noreply.github.com>
2019-02-18 16:21:33 -0600

    Update mainshell.c

--- mainshell.c
+++ mainshell.c
@@ -49,15 +49,13 @@ void tokenizer(char c[]);
 int main()
 {
     char* input;
-    // struct ShellCommand c;
 
     chdir(getenv("HOME"));
-    //printf("\n%s$ ", get_directory());
+
     while(1)
     {
         printf("\n%s$ ", get_directory());
         input = get_user_input();
-        // printf("INPUT IS: %s", input);
         execute_command(parse_input(input));
     }
 
@@ -103,22 +101,24 @@ char* get_user_input()
 ************************************/
 struct ShellCommand parse_input(char c[])
 {
-    int is_first_in_string = 1; // this to ensure that the first word is treated as a command
-
     struct ShellCommand line;  // struct that will be created with parsed input
     char *token; // = malloc(MAX_SIZE);
     const char n[1] = " ";  // splits token by spaces
+
     token = strtok(c, n);
+
     // handles the case of no input
     if(token == NULL)
     {
         line.commands[0] = "empty";
         return line;
     }
+
     line.commands[0] = malloc(sizeof(token));
     strcpy(line.commands[0], token);
     int i, j;   // counters to be used in loop
     i = 1; j = 0; // i is set at one because commands[0] is already assigned
+
     while (token != NULL)
     {
         line.commands[i] = malloc(sizeof(token)); // allocates memory
@@ -127,31 +127,17 @@ struct ShellCommand parse_input(char c[])
         if(strchr(token, '-') != NULL)  // this is causing a segmentation fault
         {
             strcpy(line.commands[i], token);
-            // printf("Token: %s contains -\n", line.commands[i]);
             i++;
         }
+
         // adds everything to arguments
         strcpy(line.arguments[j], token);
         j++;
-
         token = strtok(NULL, n);
-        is_first_in_string = 0;
     }
 
-    // line.commands[i] = NULL; // THIS IS WHAT FIXED EVERYTHING
+    // THIS IS WHAT FIXED EVERYTHING
     line.arguments[j] = NULL;
-    // printf("Commands: ");
-    // for (int n = 0; n < i; n++)
-    // {
-    //     printf("%s, ", line.commands[n]);
-    //
-    // }
-    // printf("\nArguments: ");
-    // for (int k = 0; k < j; k++)
-    // {
-    //     printf("%s, ", line.arguments[k]);
-    // }
-    // printf("\n");
 
     return line;
 }
@@ -167,10 +153,13 @@ void execute_command(struct ShellCommand s)
     {
         printf("\n");
     }
+
     if(strcmp(s.commands[0], "cd") == 0)
     {
         chdir(s.arguments[1]);
     }
+
+
     if(strcmp(s.commands[0], "exit") == 0)
     {
         exit(0);
@@ -179,6 +168,7 @@ void execute_command(struct ShellCommand s)
     {
         printf("%s", get_directory());
     }
+
     /********************************************
     * Figuring out how to get this part to work
     * caused me to rethink my entire existence.
@@ -187,6 +177,7 @@ void execute_command(struct ShellCommand s)
     else
     {
         pid_t pid = fork();
+
         if (pid == 0)
         {
             execvp(s.commands[0], s.arguments);
@@ -212,31 +203,4 @@ void execute_command(struct ShellCommand s)
             }
         }
     }
-}
-
-/***********************************
-* This function will not be
-* implemented. It is just used to
-* test how strings can be split up.
-*
-* These tests will be used to
-* implement the parse_input()
-* function.
-***********************************/
-void tokenizer(char c[])
-{
-    char *token;
-    const char n[1] = " ";  // splits token by spaces
-    token = strtok(c, n);
-    int count = 0;  // counts the tokens
-
-    printf("Token(s):\n");
-
-    while (token != NULL)
-    {
-        printf(" %s\n", token);
-        token = strtok(NULL, n);
-        count++;    // adds one to token count
-    }
-    printf("%d token(s) read\n\n", count);
 }
